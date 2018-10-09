ac0152b7518383de04325fabfb342198b51ae5d0
Liam Powell <powelll@iut1i-702-a19.local>
2018-10-09 14:22:09 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,51 @@
+function switchTheme(element){
+    if(element.hasClass("light")){
+        element.removeClass("light");
+        element.addClass("dark");
+    } else {
+        element.removeClass("dark");
+        element.addClass("light");
+    }
+}
+
+$(function(){
+    if($(window).scrollTop()== 0){
+        $("#backtop").hide();
+    } else {
+        $("#backtop").show();
+    }
+}
+);
+
+$("#switchmode").click(function (e) {
+    switchTheme($("#box"));
+    switchTheme($("#picture"));
+    switchTheme($("body"));
+
+    $(".btn").each(function () {
+        switchTheme($(this));
+    });
+
+    if($("#switchmode").hasClass("light")){
+        $("#switchicon").text(" Dark mode");
+        $("#switchicon").removeClass("fa-sun");
+        $("#switchicon").addClass("fa-moon");
+    } else {
+        $("#switchicon").text(" Light mode");
+        $("#switchicon").removeClass("fa-moon");
+        $("#switchicon").addClass("fa-sun");
+    }
+});
+
+$("#backtop").click(function(e){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
+
+$(window).scroll(function () {
+    if($(window).scrollTop()== 0){
+        $("#backtop").hide();
+    } else {
+        $("#backtop").show();
+    }
+});
