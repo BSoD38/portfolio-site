7b21352e0476ed4330a08b49092a4f061cee6359
BSoD38 <bsod1998@gmail.com>
2018-10-16 16:26:25 +0200

    small js tweaking and favicon + text changes

--- js/scripts.js
+++ js/scripts.js
@@ -18,12 +18,12 @@ $(function(){
         $.ajax({
             url: "https://api.openweathermap.org/data/2.5/weather?q=" + data.city + "," + data.country_code + "&appid=989c42344ea2700f01c6e5a0819d3d61" + "&units=metric",
             success: function(res){
-                $("#weather-loading").remove();
-                $("#weather-loaded").show();
                 $("#location-city").text(data.city);
                 $("#location-country").text(data.country_name);
                 $("#weather").text(res.weather[0].main);
                 $("#temperature").text(res.main.temp);
+                $("#weather-loading").remove();
+                $("#weather-loaded").show();
             },
             error: function(xhr, status, error){
                 console.log(xhr, status, error);
