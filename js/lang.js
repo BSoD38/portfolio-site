91feb70ff3c3c0187fc063dc0326883e9beb307a
Liam Powell <bsod1998@gmail.com>
2018-11-05 01:03:08 +0100

    Add multilanguage support, fixes

---
+++ js/lang.js
@@ -0,0 +1,91 @@
+const en =
+{
+    "intro": {
+        "title": "Hello there! I'm Liam Powell, a mobile and web developer.",
+        "subtitle": "This is my portfolio. You'll find information about me there."
+    },
+    "personal": {
+        "title": "Personal info",
+        "subtitle": "General info about me.",
+        "text1": "Birth date : 4th of July 1998",
+        "text2": "Living near Grenoble, France",
+        "text3": "Web and mobile developer"
+    },
+    "qualifications": {
+        "title": "Qualifications/Diplomas",
+        "subtitle": "These are french qualifications and diplomas. Please note that some of them don't have international equivalents.",
+        "text1": "Current - Degree in embedded systems and mobile phone programming",
+        "text2": "2018 - BTS SNIR (Diploma in information technology and networking)",
+        "text3": "2016 - Engineering baccalaureate"
+    },
+    "experience": {
+        "title": "Professional experience",
+        "subtitle": "These are the places where I've worked at (and are relevant to my skills).",
+        "text1": "Current - Prospectiva - Web developer (Symfony 2 and 3)",
+        "text2": "2017 - Enigmatic - Web developer (Symfony 2)"
+    },
+    "projects": {
+        "title": "Other projects",
+        "subtitle": "Things I've done or do in my free time.",
+        "text1": "Game creation with Unreal Engine 4",
+        "text2": "Chat bot for Discord using Node.js and Discord.js"
+    },
+    "weather": {
+        "title": "Weather",
+        "subtitle": "Because... Well, why not?",
+        "subtitle2": "(Please avoid reloading too often, I have limited calls to the weather API)",
+        "text1": "Weather for your location: ",
+        "text2": ", and the temperature is "
+    },
+    "menus": {
+        "switch-text": " Dark mode",
+        "back-top": " Back to top",
+        "change-lang": "Français"
+    }
+};
+
+const fr =
+{
+    "intro": {
+        "title": "Salut ! Je suis Liam Powell, un développeur web et mobile.",
+        "subtitle": "Ceci est mon portfolio. Vous aurez des infos sur moi ici."
+    },
+    "personal": {
+        "title": "Infos personnelles",
+        "subtitle": "Infos générales sur moi.",
+        "text1": "Date de naissance : 4 Juillet 1998",
+        "text2": "Vit près de Grenoble, France",
+        "text3": "Développeur web et mobile"
+    },
+    "qualifications": {
+        "title": "Qualifications/Diplômes",
+        "subtitle": "Ce sont des qualifications et diplômes français. Veuillez noter que certains d'entre eux n'ont pas d'équivalent international.",
+        "text1": "En ce moment - Licence Services Mobiles et Interfaces Nomades (SMIN)",
+        "text2": "2018 - BTS SNIR (Systèmes Numériques - Informatique et Réseau)",
+        "text3": "2016 - Bac S - Sciences de l'Ingénieur"
+    },
+    "experience": {
+        "title": "Expérience professionnelle",
+        "subtitle": "Voici les endroits auxquels j'ai travaillé (et sont en rapport avec mes compétences).",
+        "text1": "En ce moment - Prospectiva - Développeur web (Symfony 2 and 3)",
+        "text2": "2017 - Enigmatic - Développeur web (Symfony 2)"
+    },
+    "projects": {
+        "title": "Autres projets",
+        "subtitle": "Choses que j'ai faites ou que je fais durant mon temps libre.",
+        "text1": "Création de jeux avec Unreal Engine 4",
+        "text2": "Bot de chat pour Discord en utilisant Node.js et Discord.js"
+    },
+    "weather": {
+        "title": "Météo",
+        "subtitle": "Parce que... Pourquoi pas ?",
+        "subtitle2": "(Veuillez éviter de recharger la page trop souvent, j'ai un nombre d'appels limité à l'API de météo)",
+        "text1": "Météo pour votre localisation : ",
+        "text2": ", et la température est de "
+    },
+    "menus": {
+        "switch-text": " Mode sombre",
+        "back-top": " Retourner en haut",
+        "change-lang": "English"
+    }
+};
--- js/scripts.js
+++ js/scripts.js
@@ -12,6 +12,61 @@ function switchTheme(element) {
     }
 }
 
+function switchStrings(id, elements, lang)
+{
+    switch(lang){
+        case "en":
+            elements.each(function () {
+                if(!$(this).hasClass("fas") || !$(this).hasClass("not-text") || !$(this).hasClass("fab")){
+                    if($(this).attr("class") !== undefined){
+                        let classes = $(this).attr("class").split(' ');
+                        if(classes.length === 1){
+                            $(this).text(en[id][$(this).attr("class")]);
+                        } else {
+                            console.log($(this).attr("id"));
+                            if (classes.indexOf("menus") > -1) {
+                                $(this).text(en.menus[id]);
+                            }
+                        }
+                    }
+                }
+            });
+            break;
+        case "fr":
+            elements.each(function () {
+                if(!$(this).hasClass("fas") || !$(this).hasClass("not-text") || !$(this).hasClass("fab")) {
+                    if($(this).attr("class") !== undefined){
+                        let classes = $(this).attr("class").split(' ');
+                        if (classes.length === 1) {
+                            $(this).text(fr[id][$(this).attr("class")]);
+                        } else {
+                            if (classes.indexOf("menus") > -1) {
+                                $(this).text(fr.menus[id]);
+                            }
+                        }
+                    }
+                }
+            });
+            break;
+        default:
+            changeLanguage("en");
+            break;
+    }
+
+}
+
+function changeLanguage(lang){
+     switchStrings("intro", $("#intro").find("*"), lang);
+     switchStrings("personal", $("#personal").find("*"), lang);
+     switchStrings("qualifications", $("#qualifications").find("*"), lang);
+     switchStrings("experience", $("#experience").find("*"), lang);
+     switchStrings("projects", $("#projects").find("*"), lang);
+     switchStrings("weather", $("#weather").find("*"), lang);
+     switchStrings("switch-text", $("#switch-text"), lang);
+     switchStrings("back-top", $("#back-top"), lang);
+     switchStrings("change-lang", $("#change-lang"), lang);
+}
+
 $(function () {
         if ($(window).scrollTop() === 0) {
             $("#back-top").addClass("hidden");
@@ -24,7 +79,7 @@ $(function () {
                 success: function (res) {
                     $("#location-city").text(data.city);
                     $("#location-country").text(data.country_name);
-                    $("#weather").text(res.weather[0].description.charAt(0).toUpperCase() + res.weather[0].description.substr(1));
+                    $("#weather-data").text(res.weather[0].description.charAt(0).toUpperCase() + res.weather[0].description.substr(1));
                     $("#temperature").text(res.main.temp);
                     $("#weather-loading").remove();
                     $("#weather-loaded").show();
@@ -39,12 +94,26 @@ $(function () {
             switchTheme($("#box"));
             switchTheme($("#picture"));
             switchTheme($("body"));
+            let icon = $("#switch-icon");
+            $("#switch-text").text(" Light mode");
+            icon.removeClass("fa-moon");
+            icon.addClass("fa-sun");
+            $(".btn").each(function () {
+                switchTheme($(this));
+            });
         } else if (document.cookie.length === 0) {
             let hour = new Date().getHours();
             if(hour >= 18 || hour <= 5){
                 switchTheme($("#box"));
                 switchTheme($("#picture"));
                 switchTheme($("body"));
+                let icon = $("#switch-icon");
+                $("#switch-text").text(" Light mode");
+                icon.removeClass("fa-moon");
+                icon.addClass("fa-sun");
+                $(".btn").each(function () {
+                    switchTheme($(this));
+                });
             }
         }
     }
@@ -59,14 +128,16 @@ $("#switch-mode").on("click" ,function () {
         switchTheme($(this));
     });
 
+    let icon = $("#switch-icon");
+
     if ($("#switch-mode").hasClass("light")) {
         $("#switch-text").text(" Dark mode");
-        $("#switch-icon").removeClass("fa-sun");
-        $("#switch-icon").addClass("fa-moon");
+        icon.removeClass("fa-sun");
+        icon.addClass("fa-moon");
     } else {
         $("#switch-text").text(" Light mode");
-        $("#switch-icon").removeClass("fa-moon");
-        $("#switch-icon").addClass("fa-sun");
+        icon.removeClass("fa-moon");
+        icon.addClass("fa-sun");
     }
 });
 
@@ -74,6 +145,18 @@ $("#back-top").on("click", function () {
     $('html, body').animate({
         scrollTop: 0
     }, 1000);
+});
+
+$("#change-lang").on("click", function () {
+    if($(this).hasClass("en")){
+        changeLanguage("fr");
+        $(this).removeClass("en");
+        $(this).addClass("fr");
+    } else if ($(this).hasClass("fr")) {
+        changeLanguage("en");
+        $(this).removeClass("fr");
+        $(this).addClass("en");
+    }
 });
 
 $(window).on("scroll" ,function () {
